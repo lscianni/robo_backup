b673822bf940ca7d2004011c4d12b9794f8d7957
lscianni <lscianni@appliedtechres.com>
2017-12-13 12:16:16 -0500

    First commit

---
+++ robo_backup.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+
+::Backups up %Source% to %Destination% with logging and verbose output::
+::Louis Scianni 12/13/2017::
+
+SET Source=%1
+SET Destination=%2
+SET Backup_Date="%DATE:~-4%"
+SET Log_Dir="C:\_backups\log"
+SET Log_File="%Backup_Dir%\%Backup_Date%.txt"
+
+
+:main
+::Copy over the network to the server with verbose output and logging::
+
+IF NOT EXIST %Log_Dir% echo Creating Log Directory && mkdir %Log_Dir%
+
+IF /I %Source% == "m" goto menu
+ELSE (
+    robocopy  %Source% %Destination% /V /E /LOG+:%Log_File% /MOVE /TEE
+    goto eof
+)
+
+:menu
+cls
+echo ROBOBACKUP: Enter Source 
+echo.
+set /i /p sel_src=Source: 
+cls
+echo ROBOBACKUP: Enter Destination
+echo.
+set /p sel_dest=Destination:
+
+if /I %sel_src% == "exit" goto eof
+if /I %sel_src% == "quit" goto eof
+ELSE (
+    robocopy %sel_src% %sel_dest%  /V /E /LOG+:%Log_File% /MOVE /TEE
+)
+
+goto menu
+
+:eof
