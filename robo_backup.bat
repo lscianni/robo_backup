dc0c91d91028cc480b6f5409daa32cd20ed4406f
lsciani <lscianniit@gmail.com>
2019-03-10 18:39:13 -0400

    commit file

--- robo_backup.bat
+++ robo_backup.bat
@@ -1,8 +1,26 @@
 @ECHO OFF
 
+:: ----------------------------------------------------------------------------
+::    Copyright (C) 2019 Louis Scianni
+::
+::    This program is free software; you can redistribute it and/or modify
+::    it under the terms of the GNU General Public License as published by
+::    the Free Software Foundation; either version 2 of the License, or
+::    (at your option) any later version.
+::
+::    This program is distributed in the hope that it will be useful,
+::    but WITHOUT ANY WARRANTY; without even the implied warranty of
+::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::    GNU General Public License for more details.
+::
+::    You should have received a copy of the GNU General Public License along
+::    with this program; if not, write to the Free Software Foundation, Inc.,
+::    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+::    
+:: ----------------------------------------------------------------------------
+::
 ::Backups up %Source% to %Destination% with logging and verbose output::
-::Louis Scianni 12/13/2017::
-:: Released under FreeBSD license
+::12/13/2017::
 
 SET Source=%1
 SET Destination=%2
